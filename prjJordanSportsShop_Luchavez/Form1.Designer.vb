61a29c815c776a974fb3a52cacc983b3f0e66ae0
luchmewep <mrstudyfirst@gmail.com>
2020-06-08 09:30:42 +0800

    Initial commit.

---
+++ prjJordanSportsShop_Luchavez/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblAppName = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.txtYearlySalary = New System.Windows.Forms.TextBox()
+        Me.lblWholeYearSalary = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtWeeklySalary = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtBiweeklySalary = New System.Windows.Forms.TextBox()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.printMain = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.prjJordanSportsShop_Luchavez.My.Resources.Resources.jordan
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 15)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(163, 123)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblAppName
+        '
+        Me.lblAppName.AutoSize = True
+        Me.lblAppName.Font = New System.Drawing.Font("Impact", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAppName.Location = New System.Drawing.Point(191, 70)
+        Me.lblAppName.Name = "lblAppName"
+        Me.lblAppName.Size = New System.Drawing.Size(295, 39)
+        Me.lblAppName.TabIndex = 1
+        Me.lblAppName.Text = "Jordan's Sports Store"
+        Me.lblAppName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Cascadia Mono", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(253, 109)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(233, 28)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Salary Calculator"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblWholeYearSalary)
+        Me.GroupBox1.Controls.Add(Me.txtYearlySalary)
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 164)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(472, 63)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.txtBiweeklySalary)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.txtWeeklySalary)
+        Me.GroupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox2.Location = New System.Drawing.Point(14, 271)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(472, 94)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.YellowGreen
+        Me.btnCalculate.Cursor = System.Windows.Forms.Cursors.Default
+        Me.btnCalculate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCalculate.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(399, 235)
+        Me.btnCalculate.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(87, 28)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.IndianRed
+        Me.btnClear.Cursor = System.Windows.Forms.Cursors.Default
+        Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClear.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(399, 373)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(87, 28)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'txtYearlySalary
+        '
+        Me.txtYearlySalary.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtYearlySalary.Location = New System.Drawing.Point(199, 22)
+        Me.txtYearlySalary.Name = "txtYearlySalary"
+        Me.txtYearlySalary.Size = New System.Drawing.Size(200, 25)
+        Me.txtYearlySalary.TabIndex = 7
+        Me.txtYearlySalary.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblWholeYearSalary
+        '
+        Me.lblWholeYearSalary.AutoSize = True
+        Me.lblWholeYearSalary.Location = New System.Drawing.Point(103, 26)
+        Me.lblWholeYearSalary.Name = "lblWholeYearSalary"
+        Me.lblWholeYearSalary.Size = New System.Drawing.Size(86, 16)
+        Me.lblWholeYearSalary.TabIndex = 8
+        Me.lblWholeYearSalary.Text = "1-Year Salary"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(103, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 16)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Weekly"
+        '
+        'txtWeeklySalary
+        '
+        Me.txtWeeklySalary.BackColor = System.Drawing.Color.Beige
+        Me.txtWeeklySalary.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWeeklySalary.Location = New System.Drawing.Point(199, 22)
+        Me.txtWeeklySalary.Name = "txtWeeklySalary"
+        Me.txtWeeklySalary.ReadOnly = True
+        Me.txtWeeklySalary.Size = New System.Drawing.Size(200, 26)
+        Me.txtWeeklySalary.TabIndex = 9
+        Me.txtWeeklySalary.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(103, 59)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 16)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Biweekly"
+        '
+        'txtBiweeklySalary
+        '
+        Me.txtBiweeklySalary.BackColor = System.Drawing.Color.Beige
+        Me.txtBiweeklySalary.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBiweeklySalary.Location = New System.Drawing.Point(199, 54)
+        Me.txtBiweeklySalary.Name = "txtBiweeklySalary"
+        Me.txtBiweeklySalary.ReadOnly = True
+        Me.txtBiweeklySalary.Size = New System.Drawing.Size(200, 26)
+        Me.txtBiweeklySalary.TabIndex = 11
+        Me.txtBiweeklySalary.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnPrint
+        '
+        Me.btnPrint.BackColor = System.Drawing.Color.Gold
+        Me.btnPrint.Cursor = System.Windows.Forms.Cursors.Default
+        Me.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPrint.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrint.Location = New System.Drawing.Point(306, 373)
+        Me.btnPrint.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(87, 28)
+        Me.btnPrint.TabIndex = 7
+        Me.btnPrint.Text = "Print"
+        Me.btnPrint.UseVisualStyleBackColor = False
+        '
+        'printMain
+        '
+        Me.printMain.DocumentName = "document"
+        Me.printMain.Form = Me
+        Me.printMain.PrintAction = System.Drawing.Printing.PrintAction.PrintToPreview
+        Me.printMain.PrinterSettings = CType(resources.GetObject("printMain.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.printMain.PrintFileName = Nothing
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(503, 416)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblAppName)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Jordan's Sport Store"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblAppName As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblWholeYearSalary As Label
+    Friend WithEvents txtYearlySalary As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtBiweeklySalary As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtWeeklySalary As TextBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnPrint As Button
+    Friend WithEvents printMain As PowerPacks.Printing.PrintForm
+End Class
---
+++ prjJordanSportsShop_Luchavez/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim wholeSalary As Decimal = Val(txtYearlySalary.Text)
+        Dim weeklySalary As Decimal = wholeSalary / 52
+        Dim biweeklySalary As Decimal = wholeSalary / 26
+        txtWeeklySalary.Text = FormatCurrency(weeklySalary, 2)
+        txtBiweeklySalary.Text = FormatCurrency(biweeklySalary, 2)
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtYearlySalary.Clear()
+        txtWeeklySalary.Clear()
+        txtBiweeklySalary.Clear()
+        txtYearlySalary.Focus()
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs) Handles btnPrint.Click
+        btnCalculate.Hide()
+        btnPrint.Hide()
+        btnClear.Hide()
+        printMain.Print()
+        btnCalculate.Show()
+        btnPrint.Show()
+        btnClear.Show()
+    End Sub
+End Class
